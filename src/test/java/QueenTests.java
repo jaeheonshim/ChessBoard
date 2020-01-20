858cdd88ff648a70b364b7cc3a111f69d36846cb
Jaeheon Shim <git@jaeheonshim.com>
2020-01-20 14:47:11 -0500

    Add queen kill on landing test

--- src/test/java/QueenTests.java
+++ src/test/java/QueenTests.java
@@ -81,4 +81,22 @@ public class QueenTests {
 
 		  Assert.assertFalse("Queen should not land on piece of same color", testBoard.move(testBoard.getSpot(0, 0), testBoard.getSpot(0, 7)));
 	 }
+
+	 @Test
+	 public void queenShouldKillOnLanding() {
+	 	 Board testBoard = new Board();
+	 	 Spot[][] newBoard = new Spot[8][8];
+	 	 for(int i = 0; i < newBoard.length; i++) {
+	 	 	 for(int j = 0; j < newBoard[i].length; j++) {
+	 	 	 	 newBoard[i][j] = new Spot(j, i, null);
+			 }
+		 }
+
+	 	 newBoard[0][0] = new Spot(0, 0, new Queen(true));
+	 	 newBoard[5][0] = new Spot(0, 5, new Queen(false));
+
+	 	 testBoard.setBoard(newBoard);
+	 	 testBoard.move(testBoard.getSpot(0, 0), testBoard.getSpot(0, 5));
+	 	 Assert.assertTrue("Queen should kill pieces on landing", testBoard.getSpot(0, 5).getPiece().isWhite());
+	 }
 }
