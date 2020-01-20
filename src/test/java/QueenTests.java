8997561030ab3633453080239687082c3ac49f31
Jaeheon Shim <git@jaeheonshim.com>
2020-01-19 23:59:50 -0500

    Create more tests for Queen in QueenTests

--- src/test/java/QueenTests.java
+++ src/test/java/QueenTests.java
@@ -49,4 +49,36 @@ public class QueenTests {
 
 		  Assert.assertTrue("Queen should move diagonally", testBoard.move(testBoard.getSpot(0, 0), testBoard.getSpot(7, 7)));
 	 }
+
+	 @Test public void queenShouldNotJumpOverPieces () {
+		  Board testBoard = new Board();
+		  Spot[][] newBoard = new Spot[8][8];
+		  for (int i = 0; i < newBoard.length; i++) {
+				for (int j = 0; j < newBoard.length; j++) {
+					 newBoard[i][j] = new Spot(j, i, null);
+				}
+		  }
+
+		  newBoard[0][0] = new Spot(0, 0, new Queen(true));
+		  newBoard[5][0] = new Spot(0, 5, new Queen(false));
+		  testBoard.setBoard(newBoard);
+
+		  Assert.assertFalse("Queen should not jump over pieces", testBoard.move(testBoard.getSpot(0, 0), testBoard.getSpot(0, 7)));
+	 }
+
+	 @Test public void queenShouldNotLandOnPieceOfSameColor () {
+		  Board testBoard = new Board();
+		  Spot[][] newBoard = new Spot[8][8];
+		  for (int i = 0; i < newBoard.length; i++) {
+				for (int j = 0; j < newBoard.length; j++) {
+					 newBoard[i][j] = new Spot(j, i, null);
+				}
+		  }
+
+		  newBoard[0][0] = new Spot(0, 0, new Queen(true));
+		  newBoard[7][0] = new Spot(0, 7, new Queen(true));
+		  testBoard.setBoard(newBoard);
+
+		  Assert.assertFalse("Queen should not land on piece of same color", testBoard.move(testBoard.getSpot(0, 0), testBoard.getSpot(0, 7)));
+	 }
 }
