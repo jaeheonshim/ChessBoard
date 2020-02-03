9edf6121a91274dda9b2e14acbd100e12f23c8d3
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2020-02-03 09:51:46 -0500

    Document Spot class

--- src/main/java/com/jaeheonshim/chessboard/Spot.java
+++ src/main/java/com/jaeheonshim/chessboard/Spot.java
@@ -2,48 +2,71 @@ package com.jaeheonshim.chessboard;
 
 import com.jaeheonshim.chessboard.piece.Piece;
 
+/*
+Represents a spot within a Board.
+ */
 public class Spot {
-	 private Piece piece;
-	 private int x, y;
+    private Piece piece;
+    private int x, y;
 
-	 public Spot(int x, int y, Piece piece) {
-	 	 this.x = x;
-	 	 this.y = y;
-	 	 this.piece = piece;
-	 }
+    public Spot(int x, int y, Piece piece) {
+        this.x = x;
+        this.y = y;
+        this.piece = piece;
+    }
 
-	 public Piece getPiece () {
-		  return piece;
-	 }
+	/**
+	 * Returns the piece stored in this spot
+	 * @return Piece object stored in spot
+	 */
+	public Piece getPiece() {
+        return piece;
+    }
 
-	 public void setPiece (Piece piece) {
-		  this.piece = piece;
-	 }
+	/**
+	 * Sets the piece stored in the spot
+	 * @param piece piece to set field piece to
+	 */
+	public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
 
-	 public int getX () {
-		  return x;
-	 }
+	/**
+	 * Returns the x coordinate of the spot where (0, 0) is the lower left corner of the board
+	 * @return the x coordinate
+	 */
+	public int getX() {
+        return x;
+    }
 
-	 public int getY () {
-		  return y;
-	 }
+	/**
+	 * Returns the y coordinate of the spot where (0, 0) is the lower left corner of the board
+	 * @return the y coordinate
+	 */
+	public int getY() {
+        return y;
+    }
 
-	 public Square getSquare() {
-	 	for(Square square : Square.values()) {
-	 		if(square.getX() == this.x && square.getY() == this.y) {
-	 			return square;
-			}
-		}
+	/**
+	 * Returns the square enumeration associated with the spot on the board
+	 * @return Square corresponding to position of this spot on the board
+	 */
+	public Square getSquare() {
+        for (Square square : Square.values()) {
+            if (square.getX() == this.x && square.getY() == this.y) {
+                return square;
+            }
+        }
 
-	 	return null;
-	 }
+        return null;
+    }
 
-	@Override
-	public String toString() {
-	 	if(getPiece() != null) {
-			return getPiece().toString();
-		} else {
-	 		return null;
-		}
-	}
+    @Override
+    public String toString() {
+        if (getPiece() != null) {
+            return getPiece().toString();
+        } else {
+            return null;
+        }
+    }
 }
