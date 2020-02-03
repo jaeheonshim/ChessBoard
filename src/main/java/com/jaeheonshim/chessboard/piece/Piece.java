ea35552c55633c669556f99b17fbdeb413eec221
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2020-02-03 09:58:20 -0500

    Document Piece superclass

--- src/main/java/com/jaeheonshim/chessboard/piece/Piece.java
+++ src/main/java/com/jaeheonshim/chessboard/piece/Piece.java
@@ -14,25 +14,60 @@ public abstract class Piece {
         this.white = white;
     }
 
+    /**
+     * Returns whether the current piece is white.
+     * @return true if the current piece is white, false if it is black
+     */
     public boolean isWhite() {
         return white;
     }
 
+    /**
+     * Returns a boolean based on whether a certain move on the board is possible
+     * @param board Board piece is located within
+     * @param start Starting spot of piece
+     * @param end Spot to move piece to
+     * @param checkKingInCheck Whether to check if the king is in check and disallow move.
+     * @return true if move is possible, false if move is forbidden
+     */
     public boolean canMove(Board board, Spot start, Spot end, boolean checkKingInCheck) {
         this.checkKingInCheck = checkKingInCheck;
         return canMove(board, start, end);
     }
 
+    /**
+     * Returns a boolean based on whether a certain move on the board is possible
+     * @param board Board piece is located within
+     * @param start Starting spot of piece
+     * @param end Spot to move piece to
+     * @return true if move is possible, false if move is forbidden
+     */
     public abstract boolean canMove(Board board, Spot start, Spot end);
 
+    /**
+     * Returns a boolean based on whether a certain move on the board is possible
+     * @param board Board piece is located within
+     * @param end Spot to move piece to
+     * @return true if move is possible, false if move is forbidden
+     */
     public boolean canMove(Board board, Spot end) {
         return canMove(board, getSpot(board), end);
     }
 
+    /**
+     * Returns a boolean based on whether a certain move on the board is possible
+     * @param board Board piece is located within
+     * @param start Square enumeration of starting position of piece
+     * @param end Square enumeration of spot to move piece to
+     * @return true if move is possible, false if move is forbidden
+     */
     public boolean canMove(Board board, Square start, Square end) {
         return canMove(board, board.getSpot(start), board.getSpot(end));
     }
 
+    /**
+     * Sets the piece to have been moved. Necessary for pawns as they can move two spots forward on the first move.
+     */
     public void setMoved() {
         moved = true;
     }
@@ -41,10 +76,19 @@ public abstract class Piece {
         this.killed = killed;
     }
 
+    /**
+     * Returns whether piece has been previously moved
+     * @return true if piece has been moved, false if piece is in default position
+     */
     public boolean isMoved() {
         return moved;
     }
 
+    /**
+     * Returns the spot of the piece on the board
+     * @param board Board piece is located within
+     * @return Spot corresponding with position within board
+     */
     public Spot getSpot(Board board) {
         for (Spot[] spots : board.getBoard()) {
             for (Spot spot : spots) {
@@ -57,6 +101,10 @@ public abstract class Piece {
         return null;
     }
 
+    /**
+     * Returns the string representation of a piece. Should be capitalized for white pieces, lowercase for black pieces.
+     * @return
+     */
     @Override
     public abstract String toString();
 }
